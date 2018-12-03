a4266d1307fd547e1f6f9413d7a8c507e70f7840
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2018-12-03 09:05:34 +0100

    lexems

--- lexems.py
+++ lexems.py
@@ -1,30 +1,45 @@
 import ply.lex as lex
 
-tokens = (
-	'NUMBER',
-	'ADD_OP',
-	'MUL_OP',
-	'OPENBRACKET',
-	'CLOSEBRACKET'
+reserved_words = (
+    'if',
+    'elseif'
+    'else',
+    'while',
+    'for'
 )
 
-t_ADD_OP = r'(\+|-)'
-t_MUL_OP = r'(\*|\\)'
+
+tokens = (
+    'NUMBER',
+    'IDENTIFIER',
+    'BRACE_OPEN',
+    'BRACE_CLOSE',
+    'COMMENT',
+) + tuple(map(lambda s: s.upper(), reserved_words))
+
+t_BRACE_OPEN = '{'
+t_BRACE_CLOSE = '}'
+t_COMMENT = '//'
+
 
 def t_NUMBER(t):
-	r'\d+\.*\d*'
-	t.value = float(t.value)
-	return t
+    r'\d+\.*\d*'
+    t.value = float(t.value)
+    return t
+
 
 t_OPENBRACKET = r'\('
 t_CLOSEBRACKET = r'\)'
 
+
 def t_newline(t):
-	r'\n+'
-	t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
 
 t_ignore = ' \t'
 
+
 def t_error(t):
-	print("Illegal character '%s'" % t.value[0])
-	t.lexer.skip(1)
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
