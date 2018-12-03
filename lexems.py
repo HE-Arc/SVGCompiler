87939863e125bb8fc7aa9ed6bec0437c25806b4a
Raphaël Margueron <raphael.margueron@he-arc.ch>
2018-12-03 09:31:01 +0100

    lexemes 2nd part

--- lexems.py
+++ lexems.py
@@ -9,17 +9,21 @@ reserved_words = (
 	"else",
 	"while",
 	"for",
+
 	"boolean",
 	"true",
 	"false",
+
 	"integer",
 	"string",
 	"shape",
+
 	"radius",
 	"positionX",
 	"positionY",
 	"height",
 	"width",
+	
 	"color",
 	"red",
 	"green",
@@ -31,26 +35,31 @@ reserved_words = (
 
 tokens = (
 	'LINE_BREAK',
-	't_SEPARATOR',
-	'LINE_BREAK',
-	'LINE_BREAK',
-	'LINE_BREAK',
-	'LINE_BREAK',
 	'SEPARATOR',
 	'AFFECTATION',
 	'DESCRIPTION',
+
 	'BRACE_OPEN',
 	'BRACE_CLOSE',
 	'BRACKET_OPEN',
 	'BRACKET_CLOSE',
 	'SQUAREBRACKET_OPEN',
 	'SQUAREBRACKET_CLOSE',
+
+	'QUOTE',
+
+	'COLOR_HEX',
+
 	'COMMENT',
 	'DRAW',
+
 	'IGNORE',
+
 	'VARIABLE_NAME',
+
 	'NUMBER',
 ) + tuple(map(lambda s: s.upper(), reserved_words))
+
 
 t_LINE_BREAK = r';'
 t_SEPARATOR = r','
@@ -63,6 +72,10 @@ t_BRACKET_OPEN = r'\('
 t_BRACKET_CLOSE = r'\)'
 t_SQUAREBRACKET_OPEN = r'\]'
 t_SQUAREBRACKET_CLOSE = r'\]'
+
+t_QUOTE = r'\"|\''
+
+t_COLOR_HEX = r'#[0-9A-Fa-F]{6};'
 
 t_COMMENT = '//'
 t_DRAW = r'@'
