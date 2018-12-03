f58fb9434c22154274fbf72e99192c5648719dc5
Raphaël Margueron <raphael.margueron@he-arc.ch>
2018-12-03 09:10:48 +0100

    adding token

--- lexems.py
+++ lexems.py
@@ -1,11 +1,11 @@
 import ply.lex as lex
 
 reserved_words = (
-    "if",
-    "elsei"'
-    "else",
-    "while",
-    "for",
+	"if",
+	"elsei"'
+	"else",
+	"while",
+	"for",
 	"boolean",
 	"true",
 	"false",
@@ -28,49 +28,49 @@ reserved_words = (
 
 
 tokens = (
-    'NUMBER',
-    'IDENTIFIER',
-    'BRACE_OPEN',
-    'BRACE_CLOSE',
-    'COMMENT',
-	'OPENBRACKET',
-	'CLOSEBRACKET',
+	'NUMBER',
+	'IDENTIFIER',
+	'BRACE_OPEN',
+	'BRACE_CLOSE',
+	'BRACKET_OPEN',
+	'BRACKET_CLOSE',
+	'SQUAREBRACKET_OPEN',
+	'SQUAREBRACKET_CLOSE',
+	'DRAW',
+	'COMMENT',
+	'LINEBREAK',
+	'SEPARATOR'
+	'AFFECTATION'
+	'DESCRIPTION',
+	'VARIABLE_NAME',
+
 ) + tuple(map(lambda s: s.upper(), reserved_words))
 
 t_BRACE_OPEN = '{'
 t_BRACE_CLOSE = '}'
 t_COMMENT = '//'
 
-
-def t_NUMBER(t):
-    r'\d+\.*\d*'
-    t.value = float(t.value)
-    return t
-
-
-t_OPENBRACKET = r'\('
-t_CLOSEBRACKET = r'\)'
-t_OPENSQUAREBRACKET = r'\]'
-t_CLOSESQUAREBRACKET = r'\]'
-
+t_BRACKET_OPEN = r'\('
+t_BRACKET_CLOSE = r'\)'
+t_SQUAREBRACKET_OPEN = r'\]'
+t_SQUAREBRACKET_CLOSE = r'\]'
 
 t_LINEBREAK = r';'
 t_SEPARATOR = r','
 t_AFFECTATION = r'='
 t_DESCRIPTION = r':'
 
-t_VARIABLENAME = r'\$[a-zA-Z0-9]'
+t_VARIABLE_NAME = r'\$[a-zA-Z0-9]'
 
-t_DRAW = r'@' 
+t_DRAW = r'@'
+
+t_IGNORE = ' \t'
 
 def t_NUMBER(t):
 	r'\d+'
 	t.value = int(t.value)
 	return t
 
-t_ignore = ' \t'
-
-
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
+	print("Illegal character '%s'" % t.value[0])
+	t.lexer.skip(1)
