6d5cd0f03a6db9b95fdae447293d2f3d7c76d8b1
Raphaël Margueron <raphael.margueron@he-arc.ch>
2018-12-03 10:54:21 +0100

    adding lexem comments + impoving the test file

--- lexems.py
+++ lexems.py
@@ -15,15 +15,18 @@ reserved_words = (
 	"false",
 
 	"integer",
-	"string",
+
 	"shape",
+	"circle",
+	"triangle",
+	"rectangle",
 
 	"radius",
 	"positionX",
 	"positionY",
 	"height",
 	"width",
-	
+
 	"color",
 	"red",
 	"green",
@@ -33,9 +36,65 @@ reserved_words = (
 	"white"
 )
 
+def t_KEYWORD(t):
+	r'[A-Za-z_]\w*'
+	if t.value in reserved_words:
+		t.type = t.value.upper()
+		return t
+	else:
+		return t_error(t)
+
+# Separators
+t_LINE_BREAK = r';'
+t_SEPARATOR = r','
+t_AFFECTATION = r'='
+t_DESCRIPTION = r':'
+
+# Groups
+t_BRACE_OPEN = '{'
+t_BRACE_CLOSE = '}'
+t_BRACKET_OPEN = r'\('
+t_BRACKET_CLOSE = r'\)'
+t_SQUAREBRACKET_OPEN = r'\['
+t_SQUAREBRACKET_CLOSE = r'\]'
+
+t_DRAW = r'@'
+
+t_VARIABLE_NAME = r'\$[a-zA-Z0-9_]+'
+
+# Integer Arithmetic
+t_INTEGER_PLUS = r'\+'
+t_INTEGER_MINUS = r'-'
+t_INTEGER_TIMES = r'\*'
+t_INTEGER_DIVIDE = r'/'
+
+# Boolean Arithmetic
+t_BOOL_OR = r'\|\|'
+t_BOOL_AND = r'&&'
+t_BOOL_LT = r'<'
+t_BOOL_GT = r'>'
+t_BOOL_EQUAL = r'=='
+t_BOOL_NOT_EQUAL = r'!='
+
+# Values
+t_COLOR_HEX = r'\#[0-9A-Fa-f]{6};'
+
+def t_NUMBER(t):
+	r'\d+'
+	t.value = int(t.value)
+	return t
+
+t_COMMENT = r'//.*\n'
+t_ignore = ' |\t|\n'
+
+def t_error(t):
+	print("Illegal character '%s'" % t.value[0])
+	t.lexer.skip(1)
+
 tokens = (
+	'COMMENT',
+	'ignore',
 	'LINE_BREAK',
-	't_SEPARATOR',
 	'SEPARATOR',
 	'AFFECTATION',
 	'DESCRIPTION',
@@ -49,58 +108,27 @@ tokens = (
 
 	'QUOTE',
 
+	'INTEGER_PLUS',
+	'INTEGER_MINUS',
+	'INTEGER_TIMES',
+	'INTEGER_DIVIDE',
+
+	'BOOL_OR',
+	'BOOL_AND',
+	'BOOL_LT',
+	'BOOL_GT',
+	'BOOL_EQUAL',
+	'BOOL_NOT_EQUAL',
+
 	'COLOR_HEX',
 
-	'COMMENT',
 	'DRAW',
-
-	'IGNORE',
 
 	'VARIABLE_NAME',
 
 	'NUMBER',
 	'KEYWORD',
 ) + tuple(map(lambda s: s.upper(), reserved_words))
-
-
-t_LINE_BREAK = r';'
-t_SEPARATOR = r','
-t_AFFECTATION = r'='
-t_DESCRIPTION = r':'
-
-t_BRACE_OPEN = '{'
-t_BRACE_CLOSE = '}'
-t_BRACKET_OPEN = r'\('
-t_BRACKET_CLOSE = r'\)'
-t_SQUAREBRACKET_OPEN = r'\['
-t_SQUAREBRACKET_CLOSE = r'\]'
-
-t_QUOTE = r'\"|\''
-
-t_COLOR_HEX = r'\#[0-9A-Fa-f]{6};'
-
-t_COMMENT = '//'
-t_DRAW = r'@'
-
-t_ignore  = ' |\t|\n'
-
-t_VARIABLE_NAME = r'\$[a-zA-Z0-9]'
-
-def t_KEYWORD(t):
-	r'[A-Za-z_]\w*'
-	if t.value in reserved_words:
-		t.type = t.value.upper()
-	return t
-
-
-def t_NUMBER(t):
-	r'\d+'
-	t.value = int(t.value)
-	return t
-
-def t_error(t):
-	print("Illegal character '%s'" % t.value[0])
-	t.lexer.skip(1)
 
 lex.lex()
 
